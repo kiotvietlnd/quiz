b1a1d2f99e530a79e5c224bab372f983668278b4
kiotvietlnd <citigolnd@gmail.com>
2026-07-15 09:09:26 +0700

    Update codelab-data.js

--- thuc-hanh/tai-ky/codelab-data.js
+++ thuc-hanh/tai-ky/codelab-data.js
@@ -1,7 +1,7 @@
 // CẤU HÌNH HỆ THỐNG DÀNH CHO ADMIN
 const APP_CONFIG = {
     // Dán link API của Google Apps Script vào đây
-    API_URL: "https://script.google.com/macros/s/AKfycbxEY084om4aAvHj0jR2B4Sxb8gjYW1a5YsqUPporefzXYo9mfHjo4Nfq6UwiuWziadFBQ/exec", 
+    API_URL: "https://script.google.com/macros/s/AKfycbztRc3HTGfebvR4scvbzNp3-KT4LLb0GdnjsWg0O8RRifT5cBnkpYBktZpQ3HU_ks_nig/exec", 
     COURSE_NAME: "KIOT_RENEWAL_Q3_2026" 
 };
 
